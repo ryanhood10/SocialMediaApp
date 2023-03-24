8b3a9378dc5ac6706e26f93ac34477167d9ccb11
JuanMullert <114509238+JuanMullert@users.noreply.github.com>
2023-03-24 17:54:35 -0500

    working on login page

--- client/src/App.js
+++ client/src/App.js
@@ -8,43 +8,46 @@ import Login from './components/pages/Login';
 import Profile from './components/pages/Profile';
 import Search from './components/pages/Search';
 // header and footer imports
-import Header from './components/Header'
-import Footer from './components/Footer'
+// import Header from './components/Header'
+// import Footer from './components/Footer'
 
 
 function App() {
   return (
+    <>
+      <div className="App">
 
-    <div className="App">
+        <HashRouter>
+          <header className='navbarHeader'>Header Place Holder
+            <ul className="nav justify-content-center">
+              <li className="nav-item">
+                <Link className="nav-link" to='/Login'>login placeholder</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/Homepage'>homepage</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/Profile'>profile page</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/Search'>search result</Link>
+              </li>
+            </ul>
+          </header>
+          
+            <Routes>
 
-      <HashRouter>
-        <header className='navbarHeader'>Header Place Holder
-          <ul className="nav justify-content-center">
-            <li className="nav-item">
-              <Link className="nav-link" to='/'>login placeholder</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/contact'>homepage</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/blog'>profile page</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to='/projects'>search result</Link>
-            </li>
-          </ul>
-        </header>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/homepage' element={<Homepage />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/search' element={<Search />} />
-        </Routes>
-      </HashRouter>
+              <Route exact path='/' element={<Login />} />
+              <Route path='/Homepage' element={<Homepage />} />
+              <Route path='/Profile' element={<Profile />} />
+              <Route path='/Search' element={<Search />} />
+            </Routes>
+          
+        </HashRouter>
 
 
-    </div>
-
+      </div>
+    </>
 
   );
 }
--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,7 +1,102 @@
-import Signup from './pages/Signup';
+import React from 'react'
+import '../../assets/login.css'
+import Signup from '../pages/Signup'
+
+export default function Login() {
+    (
+        <div>
+<section classname="vh-100">
+  <div classname="container-fluid h-custom">
+    <div classname="row d-flex justify-content-center align-items-center h-100">
+      <div classname="col-md-9 col-lg-6 col-xl-5">
+        <img src=""></img>
+      </div>
+      <div classname="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div classname="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <p classname="lead fw-normal mb-0 me-3">Sign in with</p>
+            <button type="button" classname="btn btn-primary btn-floating mx-1">
+              <i classname="fab fa-facebook-f"></i>
+            </button>
+
+            <button type="button" classname="btn btn-primary btn-floating mx-1">
+              <i classname="fab fa-twitter"></i>
+            </button>
+
+            <button type="button" classname="btn btn-primary btn-floating mx-1">
+              <i classname="fab fa-linkedin-in"></i>
+            </button>
+          </div>
+
+          <div classname="divider d-flex align-items-center my-4">
+            <p classname="text-center fw-bold mx-3 mb-0">Or</p>
+          </div>
+
+          
+          <div classname="form-outline mb-4">
+            <input type="email" id="form3Example3" classname="form-control form-control-lg"
+              placeholder="Enter a valid email address" />
+            <label classname="form-label" for="form3Example3">Email address</label>
+          </div>
+
+          
+          <div classname="form-outline mb-3">
+            <input type="password" id="form3Example4" classname="form-control form-control-lg"
+              placeholder="Enter password" />
+            <label classname="form-label" for="form3Example4">Password</label>
+          </div>
+
+          <div classname="d-flex justify-content-between align-items-center">
+            
+            <div classname="form-check mb-0">
+              <input classname="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+              <label classname="form-check-label" for="form2Example3">
+                Remember me
+              </label>
+            </div>
+            <a href="#!" classname="text-body">Forgot password?</a>
+          </div>
+
+          <div classname="text-center text-lg-start mt-4 pt-2">
+            <button type="button" classname="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+            <p classname="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
+             classname="link-danger">Register</a></p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+  <div
+ classname="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    
+    <div classname="text-white mb-3 mb-md-0">
+     <p>Copyright © 2020. All rights reserved.</p> 
+    </div>
+   
+
+    
+    <div>
+      <a href="#!" classname="text-white me-4">
+        <i classname="fab fa-facebook-f"></i>
+      </a>
+      <a href="#!" classname="text-white me-4">
+        <i classname="fab fa-twitter"></i>
+      </a>
+      <a href="#!" classname="text-white me-4">
+        <i classname="fab fa-google"></i>
+      </a>
+      <a href="#!" classname="text-white">
+        <i classname="fab fa-linkedin-in"></i>
+      </a>
+    </div>
+    
+  </div>
+</section>
+
+</div>
+    )
+}
 
 
-
-
-
-export default Login
