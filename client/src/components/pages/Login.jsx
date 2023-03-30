2c5ca6411965760aaacbf348945c13f4432b18b7
Andrew <andrewy3295@gmail.com>
2023-03-29 19:42:45 -0500

    signup page works

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
   return (
 
     <div className='loginBody'>
-   
+
       <section className="vh-100">
         <div className="container-fluid h-custom">
           <div className="row d-flex justify-content-center align-items-center h-100">
@@ -22,22 +22,22 @@ export default function Login() {
               <main className='main'>
                 <form className='theform'>
                   <div className="form-outline mb-4">
-                    <input 
-                    type="email" 
-                    id="form3Example3" 
-                    className="form-control form-control-lg"
-                    placeholder="Enter a valid email address"
+                    <input
+                      type="email"
+                      id="form3Example3"
+                      className="form-control form-control-lg"
+                      placeholder="Enter a valid email address"
                     />
                     <label className="form-label" htmlFor="form3Example3"></label>
                   </div>
 
 
                   <div className="form-outline mb-3">
-                    <input 
-                    type="password" 
-                    id="form3Example4" 
-                    className="form-control form-control-lg"
-                    placeholder="Enter password" 
+                    <input
+                      type="password"
+                      id="form3Example4"
+                      className="form-control form-control-lg"
+                      placeholder="Enter password"
                     />
                     <label className="form-label" htmlFor="form3Example4"></label>
                   </div>
@@ -45,11 +45,11 @@ export default function Login() {
                   <div className="d-flex justify-content-between align-items-center">
 
                     <div className="form-check mb-0">
-                      <input 
-                      className="form-check-input me-2" 
-                      type="checkbox" 
-                      value="" 
-                      id="form2Example3" />
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        value=""
+                        id="form2Example3" />
                       <label className="form-check-label" htmlFor="form2Example3">
                         Remember me
                       </label>
@@ -60,9 +60,9 @@ export default function Login() {
                   <div className="text-center text-lg-start mt-4 pt-2">
                     <button type="button" className="btn btn-primary btn-lg btnlog"
                     >Login</button>
-                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account?  
-                    <Link to='/Signup' className="link-danger"> Register</Link></p>
-                    
+                    <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account?
+                      <Link to='/Signup' className="link-danger"> Register</Link></p>
+
 
 
                   </div>
@@ -83,7 +83,7 @@ export default function Login() {
         </footer>
       </section>
 
-   
+
 
     </div>
 
